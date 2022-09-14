3b0b64ec9b975e53e11ff716990c06432922f909
Andreas Haug <a-haug@hotmail.com>
2022-09-14 13:44:45 +0200

    cleanup

--- datastructure/src/closest.rs
+++ datastructure/src/closest.rs
@@ -1,32 +1,3 @@
-//implement closest
-//find closest link
-//find list of coordinates
-//estimate closest coordinate, get index
-
-//idea:
-//if closest is a node, that is, if closest point is either first or
-// last of the linestring, we add this node to Dijkstra queue with
-// cost 0
-
-//else we find distance on linestring in both directions we
-// can drive and add to Dijkstra queue with cost = distance from
-// startpoint. Must also check which directions one can drive.
-
-//need:
-// - distance of linestring formula
-// - distance between points formula
-
-// create a query function in load_mongodb
-// takes a collection name
-// do the query, return a cursor
-
-//we query with find closest, get a link
-//find closest point on the linestring
-//find index
-//can this be done in a mongodb query?
-//use some $function, map to distance from point and then return min
-// of these distances
-
 use crate::mongodb_queries;
 use serde::{Deserialize, Serialize};
 
@@ -45,12 +16,8 @@ pub struct Closest {
     pub node: Option<String>,
     pub linestring_index: usize,
     pub linestring_length: usize,
-    
-    // pub start: [f64; 2],
-    // pub reference: String,
-    // pub linestring_index: i32,
-    // pub linestring_length: i32,
 }
+
 
 impl Closest {
     pub fn link(&self) -> &String {
@@ -73,5 +40,3 @@ impl Closest {
 	}
     }
 }
-// finn en referanse
-// fn haversine_distance(p1: [f64; 2], p2: [f64; 2]) {}
