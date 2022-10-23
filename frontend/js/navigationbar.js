efd35dd68af70a206ffa408f51f3f1ec47e92675
Andreas Haug <a-haug@hotmail.com>
2022-10-23 12:28:27 +0200

    fix time selection visualization

--- frontend/js/navigationbar.js
+++ frontend/js/navigationbar.js
@@ -26,13 +26,39 @@ function changeBoth(e) {
 }
 
 
+async function setMinutes(e, m) {
+    m.target.classList.add("clicked");
+    if (prevMinutes !== null) {
+	prevMinutes.classList.remove("clicked");
+    }
+    prevMinutes = m.target;
+    cost = e;
+    await draw();
+}
+
+
 let polygonbutton = document.getElementById("polbutton"); 
 let linebutton = document.getElementById("linbutton"); 
 let bothbutton = document.getElementById("bothbutton");
 let timebutton = document.getElementById("timebutton");
 polygonbutton.classList.add("clicked") 
-let prev = polygonbutton; 
+let prev = polygonbutton;
 polygonbutton.addEventListener("click", changePolygon); 
 linebutton.addEventListener("click", changeLinestrings); 
-bothbutton.addEventListener("click", changeBoth); 
-// timebutton.addEventListener("click", )
+bothbutton.addEventListener("click", changeBoth);
+
+let button1min = document.getElementById("1min");
+button1min.classList.add("clicked");
+
+let button2min = document.getElementById("2min");
+let button5min = document.getElementById("5min");
+let button10min = document.getElementById("10min");
+let button20min = document.getElementById("20min");
+
+button1min.addEventListener("click", (m) => { setMinutes(1, m);  });
+button2min.addEventListener("click", (m) => { setMinutes(2, m);  });
+button5min.addEventListener("click", (m) => { setMinutes(5, m);  });
+button10min.addEventListener("click", (m) => { setMinutes(10, m);  });
+button20min.addEventListener("click", (m) => { setMinutes(20, m);  });
+
+let prevMinutes = button1min;
