2f42f0f2cac9e433bdf0e69a172ee91183cb0c21
Andreas Haug <a-haug@hotmail.com>
2022-10-23 21:38:38 +0200

    delete

--- dbs/run_neo4j.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-       -p 7474:7474 -p 7687:7687 \
-       --rm \
-       -v $(pwd)/neo4j/data:/data \
-       -v $(pwd)/neo4j/logs:/logs \
-       -v $(pwd)/neo4j/import:/var/lib/neo4j/import \
-       -v $(pwd)/neo4j/plugins:/plugins \
-       --env NEO4J_AUTH=neo4j/neopassword \
-       neo4j:4.4.0
